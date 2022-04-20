4b1a0097eed53887babda92d58b732ba0a22c897
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-04-20 13:06:49 +0300

    Updated dictionary dependency.

--- include/Word.h
+++ include/Word.h
@@ -20,6 +20,9 @@ public:
     Word clone();
     string getName() const;
     void setName(string name);
+    static string beforeLastVowel(string stem);
+    static string lastVowel(string stem);
+    static string lastPhoneme(string stem);
     static bool isCapital(string surfaceForm);
     static bool isPunctuation(string surfaceForm);
     static bool isHonorific(string surfaceForm);
@@ -29,13 +32,30 @@ public:
     static bool endsWith(string largeString, string smallString);
     static vector<string> split(string line, string separator);
     static vector<string> split(string line);
+    static string replaceAll(string str, string from, string to);
+    static string trim(string str);
     static int size(string surfaceForm);
+    static string toLowerCase(string surfaceForm);
+    static string toUpperCase(string surfaceForm);
+    static string lastChar(string surfaceForm);
     static string charAt(string surfaceForm, int index);
+    static string* allCharacters(string surfaceForm);
     static string substring(string surfaceForm, int index, int length);
+    static string substring(string surfaceForm, int index);
+    static string substringExceptLastChar(string surfaceForm);
+    static string substringExceptLastTwoChars(string surfaceForm);
     bool isPunctuation();
     static bool isTime(string surfaceForm);
     static Word* toWordArray(string* sourceArray, int size);
     vector<Word> toCharacters();
+    friend istream& operator>> (istream& is, Word& word){
+        is >> word.name;
+        return is;
+    }
+    friend ostream& operator<<(ostream& os, const Word& word){
+        os << word.getName();
+        return os;
+    }
     bool operator==(const Word &anotherWord) const{
         return (name == anotherWord.name);
     }
